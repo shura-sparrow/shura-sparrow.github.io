67e42e311ed454a41337bf54d62063e3a3cfb3b9
Alexandra Vorobyova <shura.sparrow@gmail.com>
2015-11-29 00:10:10 +0300

    new velosite pages

---
+++ velosite/assets/scripts/load.php
@@ -0,0 +1,181 @@
+
+<div class="card card--loading">
+		<div class="card--inner">
+			<!-- show rating badge if there is one-->
+			<div class="card_badge card_badge--rating">ХИТ
+				<svg class="icon accent-white">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_star"></use>
+				</svg><span>4,5</span>
+			</div>
+			<!-- show discrount badge if there is one-->
+			<div class="card_badge card_badge--on-sale"><span>SALE&nbsp;</span><span>25%</span>
+			</div>
+			<div class="card__buttons">
+				<div class="card_btn card_btn--compare">
+					<a href="#">
+						<!-- i.icon-stats-->
+						<svg class="icon accent-chocolate">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_stats"></use>
+						</svg>
+					</a>
+				</div>
+				<div class="card_btn card_btn--favorite">
+					<a href="#">
+						<!-- i.icon-heart-->
+						<svg class="icon accent-chocolate">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_heart"></use>
+						</svg>
+					</a>
+				</div>
+			</div>
+			<div class="card__reviews--hot"><a href="#">Отзывов:&nbsp;<span>7</span></a>
+			</div>
+			<div class="card_photo">
+				<img src="./assets/images/velo-medium.jpg">
+			</div>
+			<div class="card__info"><span class="card__info_category">Горный велосипед&nbsp;</span><span class="card__info_year accent-red">2015</span>
+				<div class="card__info_name">MERIDA BIG.SEVEN 40-MD</div>
+			</div>
+			<hr class="card__separator">
+			<div class="card__bottom">
+				<div class="card__bottom_price-wrapper">
+					<div class="card__bottom_price--left global-grid-50 grid-parent">
+						<!-- show card old price, if discount is true-->
+						<div class="card__bottom_old-price"><span class="card_strike">30 000</span>
+						</div>
+						<div class="card__bottom_price"><span class="accent-red">25 700</span><span class="card__bottom_currency">&nbsp;Руб.</span>
+						</div>
+					</div>
+					<div class="card__bottom_price--right global-grid-50 grid-parent">
+						<button class="form-control form-control-light">Купить</button>
+					</div>
+				</div>
+				<div class="card__bottom__in-stock">
+					<!-- img(src='./assets/images/icons/check52.svg')-->
+					<svg class="icon accent-green">
+						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_check"></use>
+					</svg><span>Есть в наличии</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="card card--loading">
+    		<div class="card--inner">
+    			<!-- show rating badge if there is one-->
+    			<div class="card_badge card_badge--rating">ХИТ
+    				<svg class="icon accent-white">
+    					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_star"></use>
+    				</svg><span>4,5</span>
+    			</div>
+    			<!-- show discrount badge if there is one-->
+    			<div class="card_badge card_badge--on-sale"><span>SALE&nbsp;</span><span>25%</span>
+    			</div>
+    			<div class="card__buttons">
+    				<div class="card_btn card_btn--compare">
+    					<a href="#">
+    						<!-- i.icon-stats-->
+    						<svg class="icon accent-chocolate">
+    							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_stats"></use>
+    						</svg>
+    					</a>
+    				</div>
+    				<div class="card_btn card_btn--favorite">
+    					<a href="#">
+    						<!-- i.icon-heart-->
+    						<svg class="icon accent-chocolate">
+    							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_heart"></use>
+    						</svg>
+    					</a>
+    				</div>
+    			</div>
+    			<div class="card__reviews--hot"><a href="#">Отзывов:&nbsp;<span>7</span></a>
+    			</div>
+    			<div class="card_photo">
+    				<img src="./assets/images/velo-medium.jpg">
+    			</div>
+    			<div class="card__info"><span class="card__info_category">Горный велосипед&nbsp;</span><span class="card__info_year accent-red">2015</span>
+    				<div class="card__info_name">MERIDA BIG.SEVEN 40-MD</div>
+    			</div>
+    			<hr class="card__separator">
+    			<div class="card__bottom">
+    				<div class="card__bottom_price-wrapper">
+    					<div class="card__bottom_price--left global-grid-50 grid-parent">
+    						<!-- show card old price, if discount is true-->
+    						<div class="card__bottom_old-price"><span class="card_strike">30 000</span>
+    						</div>
+    						<div class="card__bottom_price"><span class="accent-red">25 700</span><span class="card__bottom_currency">&nbsp;Руб.</span>
+    						</div>
+    					</div>
+    					<div class="card__bottom_price--right global-grid-50 grid-parent">
+    						<button class="form-control form-control-light">Купить</button>
+    					</div>
+    				</div>
+    				<div class="card__bottom__in-stock">
+    					<!-- img(src='./assets/images/icons/check52.svg')-->
+    					<svg class="icon accent-green">
+    						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_check"></use>
+    					</svg><span>Есть в наличии</span>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<div class="card card--loading">
+        		<div class="card--inner">
+        			<!-- show rating badge if there is one-->
+        			<div class="card_badge card_badge--rating">ХИТ
+        				<svg class="icon accent-white">
+        					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_star"></use>
+        				</svg><span>4,5</span>
+        			</div>
+        			<!-- show discrount badge if there is one-->
+        			<div class="card_badge card_badge--on-sale"><span>SALE&nbsp;</span><span>25%</span>
+        			</div>
+        			<div class="card__buttons">
+        				<div class="card_btn card_btn--compare">
+        					<a href="#">
+        						<!-- i.icon-stats-->
+        						<svg class="icon accent-chocolate">
+        							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_stats"></use>
+        						</svg>
+        					</a>
+        				</div>
+        				<div class="card_btn card_btn--favorite">
+        					<a href="#">
+        						<!-- i.icon-heart-->
+        						<svg class="icon accent-chocolate">
+        							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_heart"></use>
+        						</svg>
+        					</a>
+        				</div>
+        			</div>
+        			<div class="card__reviews--hot"><a href="#">Отзывов:&nbsp;<span>7</span></a>
+        			</div>
+        			<div class="card_photo">
+        				<img src="./assets/images/velo-medium.jpg">
+        			</div>
+        			<div class="card__info"><span class="card__info_category">Горный велосипед&nbsp;</span><span class="card__info_year accent-red">2015</span>
+        				<div class="card__info_name">MERIDA BIG.SEVEN 40-MD</div>
+        			</div>
+        			<hr class="card__separator">
+        			<div class="card__bottom">
+        				<div class="card__bottom_price-wrapper">
+        					<div class="card__bottom_price--left global-grid-50 grid-parent">
+        						<!-- show card old price, if discount is true-->
+        						<div class="card__bottom_old-price"><span class="card_strike">30 000</span>
+        						</div>
+        						<div class="card__bottom_price"><span class="accent-red">25 700</span><span class="card__bottom_currency">&nbsp;Руб.</span>
+        						</div>
+        					</div>
+        					<div class="card__bottom_price--right global-grid-50 grid-parent">
+        						<button class="form-control form-control-light">Купить</button>
+        					</div>
+        				</div>
+        				<div class="card__bottom__in-stock">
+        					<!-- img(src='./assets/images/icons/check52.svg')-->
+        					<svg class="icon accent-green">
+        						<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../assets/images/icon.svg#icon_check"></use>
+        					</svg><span>Есть в наличии</span>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
---
+++ velosite/assets/scripts/video.php
@@ -0,0 +1,51 @@
+<div class="card card--double-width hide-on-mobile card--small card--loading">
+	<div class="card--inner">
+		<div class="card__video_preview">
+			<div class="card__video_preview_img">
+				<div class="youtube-container">
+					<div data-id="7ZmJtYaUTa0" data-title="Video title #01" class="youtube-player"></div>
+				</div>
+			</div>
+		</div>
+		<div class="card__video_note"><a href="#">Катание на горном велосипеде. Обзорное видео.</a></div>
+	</div>
+</div>
+<div class="card card--double-width hide-on-mobile card--small card--loading">
+	<div class="card--inner">
+		<div class="card__video_preview">
+			<div class="card__video_preview_img">
+				<div class="youtube-container">
+					<div data-id="7ZmJtYaUTa0" data-title="Video title #02" class="youtube-player"></div>
+				</div>
+			</div>
+		</div>
+		<div class="card__video_note"><a href="#">Катание на горном велосипеде. Обзорное видео.</a>
+		</div>
+	</div>
+</div>
+<div class="card card--double-width hide-on-mobile card--small card--loading">
+	<div class="card--inner">
+		<div class="card__video_preview">
+			<div class="card__video_preview_img">
+				<div class="youtube-container">
+					<div data-id="7ZmJtYaUTa0" data-title="Video title #03" class="youtube-player"></div>
+				</div>
+			</div>
+		</div>
+		<div class="card__video_note"><a href="#">Катание на горном велосипеде. Обзорное видео.</a>
+		</div>
+	</div>
+</div>
+<div class="card card--double-width hide-on-mobile card--small card--loading">
+	<div class="card--inner">
+		<div class="card__video_preview">
+			<div class="card__video_preview_img">
+				<div class="youtube-container">
+					<div data-id="7ZmJtYaUTa0" data-title="Video title #04" class="youtube-player"></div>
+				</div>
+			</div>
+		</div>
+		<div class="card__video_note"><a href="#">Катание на горном велосипеде. Обзорное видео.</a>
+		</div>
+	</div>
+</div>
